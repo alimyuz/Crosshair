1899a90a110c9c8c6a8b7e1ca6475fff5cbd2c06
Alim Yuzbashev <45521753+alimyuz@users.noreply.github.com>
2022-05-16 13:15:05 +0200

    update

--- Crosshair/AppDelegate.swift
+++ Crosshair/AppDelegate.swift
@@ -11,7 +11,6 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     var overlayWindow: NSWindow!
-    // 1
     private var statusItem: NSStatusItem!
     private var popover: NSPopover!
     
@@ -19,10 +18,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let frame = NSScreen.main!.frame
         
         overlayWindow = NSWindow(contentRect: frame, styleMask: .borderless, backing: .buffered, defer: false)
-        //overlayWindow.level =  //NSWindow.Level(rawValue: Int(CGWindowLevelForKey()))
-        overlayWindow.collectionBehavior = [.canJoinAllSpaces, .fullScreenPrimary]
+        overlayWindow.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
         overlayWindow.contentViewController = OverlayViewController()
         overlayWindow.ignoresMouseEvents = true
+        overlayWindow.level = .floating
         
         overlayWindow.makeKeyAndOrderFront(nil)
         
@@ -37,8 +36,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         
         if let button = statusItem.button {
-            button.image = NSImage(systemSymbolName: "1.circle", accessibilityDescription: "1")
-            button.action = #selector(statusItemToggle)
+            button.image = NSImage(systemSymbolName: "xmark.circle", accessibilityDescription: "crosshair app")
+            button.action = #selector(statusItemClick)
         }
     }
     
@@ -51,7 +50,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @objc
-    private func statusItemToggle(_ sender: NSStatusBarButton) {
+    private func statusItemClick(_ sender: NSStatusBarButton) {
         self.popover.show(relativeTo: sender.bounds, of: sender, preferredEdge: NSRectEdge.minY)
     }
 }
